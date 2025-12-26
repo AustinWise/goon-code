bf739663d642cb52456e4b4a5335cd9be382c513
Austin Wise <AustinWise@gmail.com>
2025-12-26 23:08:53 +0000

    Add some options to break down what is contributing to bundle size
    It would be nice if you could view the tree-shaking graph.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -10,13 +11,20 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      build: {
+        sourcemap: true,
+        // sourcemapFile: 'source.map'
+      },
       plugins: [
         react(),
-        tailwindcss()
+        tailwindcss(),
+        visualizer({
+          emitFile: true,
+          filename: 'stats.html',
+          sourcemap: true,
+        }),
       ],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
